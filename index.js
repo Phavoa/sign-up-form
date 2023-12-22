51fc40ff34eb95e4dc7c54c1a3720a3377a11479
Efemiaya Favour Oghenetega <EfemiayaFavour@gmail.com>
2023-12-22 15:00:17 +0100

    feat: Implement form validation and styling
    - Added JavaScript functionality for password matching
    - Styled form inputs for better user experience
    - Included form validation for all input fields
    - Updated font style for improved aesthetics
    - Ensured responsiveness in design

---
+++ index.js
@@ -0,0 +1,31 @@
+const password = document.querySelector("#password");
+const comPassword = document.querySelector("#com-password");
+const button = document.querySelector("#submitButton");
+const inputs = document.querySelectorAll("input");
+
+button.addEventListener("click", function (event) {
+  
+  if (password.value !== comPassword.value) {
+    const newErrorMessage = document.createElement("p");
+    newErrorMessage.className = "errormessage";
+    newErrorMessage.textContent = "*Passwords do not match";
+    comPassword.parentElement.appendChild(newErrorMessage);
+
+    event.preventDefault();
+    password.style.border = "2px solid red";
+    comPassword.style.border = "2px solid red";
+    newErrorMessage.style.color = "red";
+  }
+});
+
+for (let i = 0; i < inputs.length; i++) {
+  inputs[i].addEventListener('input', function() {
+    if (!inputs[i].checkValidity()) {
+      inputs[i].style.border = '2px solid red';
+    } else {
+      inputs[i].style.border = '2px solid green'; // Optionally, reset border for valid input
+    }
+  });
+}
+
+
