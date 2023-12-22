e0779999d7a7ceb36176e7576c26a5febda5088a
Efemiaya Favour Oghenetega <EfemiayaFavour@gmail.com>
2023-12-22 15:31:58 +0100

    improved on phone number input by adding number validation with javascript

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const password = document.querySelector("#password");
 const comPassword = document.querySelector("#com-password");
 const button = document.querySelector("#submitButton");
 const inputs = document.querySelectorAll("input");
+const phoneInput = document.querySelector("#phone");
 
 button.addEventListener("click", function (event) {
   
@@ -23,9 +24,25 @@ for (let i = 0; i < inputs.length; i++) {
     if (!inputs[i].checkValidity()) {
       inputs[i].style.border = '2px solid red';
     } else {
-      inputs[i].style.border = '2px solid green'; // Optionally, reset border for valid input
+      inputs[i].style.border = '2px solid green';
     }
   });
 }
 
+phoneInput.addEventListener("input", function () {
+  validatePhoneNumber();
+});
+
+function validatePhoneNumber() {
+  const phoneNumber = phoneInput.value;
+  const phoneNumberPattern = /^[+\d]+$/;
+
+  if (phoneNumberPattern.test(phoneNumber)) {
+    phoneInput.setCustomValidity("");
+    phoneInput.style.border = '2px solid green';
+  } else {
+    phoneInput.setCustomValidity("Please enter a valid phone number");
+    phoneInput.style.border = '2px solid red';
+  }
+}
 
